8709995c7c57f9899a1272ea9b5107af486b529d
Henrique Sebastião <contato@henriquesebastiao.com>
2026-05-30 03:59:44 -0400

    test: Testando rota de atualização de contas - close #2

--- tests/integration/test_accounts_route.py
+++ tests/integration/test_accounts_route.py
@@ -1,3 +1,4 @@
+
 from fastapi import status
 
 
@@ -54,6 +55,64 @@ async def test_route_create_account_already_exists(client):
         json={
             'name': 'Nubank',
             'balance': 200.00,
+        },
+    )
+
+    assert response.status_code == status.HTTP_409_CONFLICT
+    assert response.json()['detail'] == 'Account already exists'
+
+
+async def test_route_update_account_name(client, account):
+    response = await client.patch(
+        f'/accounts/{account.id}',
+        json={
+            'name': 'Sicredi',
+        },
+    )
+
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json()['name'] == 'Sicredi'
+    assert response.json()['balance'] == '150.00'
+
+
+async def test_route_update_account_balance(client, account):
+    response = await client.patch(
+        f'/accounts/{account.id}',
+        json={
+            'balance': '200.00',
+        },
+    )
+
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json()['name'] == account.name
+    assert response.json()['balance'] == '200.00'
+
+
+async def test_route_update_account_does_not_exist(client):
+    response = await client.patch(
+        '/accounts/10',
+        json={
+            'name': 'Conta Inexistente',
+        },
+    )
+
+    assert response.status_code == status.HTTP_404_NOT_FOUND
+    assert response.json()['detail'] == 'Account does not exist'
+
+
+async def test_route_update_account_already_exists(client, account):
+    await client.post(
+        '/accounts',
+        json={
+            'name': 'Nubank',
+            'balance': 100.00,
+        },
+    )
+
+    response = await client.patch(
+        f'/accounts/{account.id}',
+        json={
+            'name': 'Nubank',
         },
     )
 
